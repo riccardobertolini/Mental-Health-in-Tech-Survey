c578d754d09a5f649582af3d7849bda44743978f
riccardobertolini <info@riccardobertolini.net>
2023-07-28 14:07:06 +0200

    refactor: adjust the principal component label

--- clustering.py
+++ clustering.py
@@ -11,23 +11,17 @@ def perform_clustering(df):
     # Convert categorical variables to integers
     df_encoded = df.apply(lambda x: pd.factorize(x)[0])
 
-    # Perform scaling
     scaler = StandardScaler()
     df_scaled = scaler.fit_transform(df_encoded)
 
-    # Perform PCA
     pca = PCA(n_components=2)
     principalComponents = pca.fit_transform(df_scaled)
-    principalDf = pd.DataFrame(data=principalComponents, columns=['principal component 1', 'principal component 2'])
+    principalDf = pd.DataFrame(data=principalComponents, columns=['Socio-Economic Profile', 'Demographic Characteristics'])
 
-    # Perform K-means clustering
     kmeans = KMeans(n_clusters=3)
     clusters = kmeans.fit_predict(principalDf)
-
-    # Add cluster labels to the original DataFrame
     df['cluster'] = clusters
 
-    # Plot the clusters
     plt.figure(figsize=(8, 6))
     plt.scatter(principalComponents[:, 0], principalComponents[:, 1], c=clusters, cmap='viridis')
     plt.xlabel('Principal Component 1')
@@ -35,7 +29,6 @@ def perform_clustering(df):
     plt.title('Clusters')
     plt.show()
 
-    # Plot the distributions of 'remote_work' and 'mental_health_consequence' within each cluster
     for i in range(3):
         plt.figure(figsize=(12, 6))
         plt.subplot(1, 2, 1)
@@ -47,7 +40,6 @@ def perform_clustering(df):
         plt.tight_layout()
         plt.show()
 
-    # Perform Chi-square tests and plot contingency tables
     for i in range(3):
         table = pd.crosstab(df[df['cluster'] == i]['remote_work'], df[df['cluster'] == i]['mental_health_consequence'])
         chi2, p, dof, ex = chi2_contingency(table)
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from scipy.stats import chi2_contingency
 from clustering import perform_clustering
 from visualization import visualize_distributions
 
+
 def chi_square_test(df_cluster, cluster):
     contingency_table = pd.crosstab(df_cluster['remote_work'], df_cluster['mental_health_consequence'])
     print("Cluster " + str(cluster) + " contingency table:")
@@ -12,14 +13,15 @@ def chi_square_test(df_cluster, cluster):
     chi2, p, _, _ = chi2_contingency(contingency_table)
     print("Chi-square Test for Cluster " + str(cluster) + ": Chi2=" + str(chi2) + ", p=" + str(p))
 
+
 def main():
     df = pd.read_csv('data/survey.csv')
 
     df_clustered, principal_df = perform_clustering(df)
 
-    # Visualization of Clusters
-    plt.figure(figsize=(8,6))
-    sns.scatterplot(data=principal_df, x='principal component 1', y='principal component 2', hue=df_clustered['Cluster'])
+    plt.figure(figsize=(8, 6))
+    sns.scatterplot(data=principal_df, x='Socio-Economic Profile', y='Demographic Characteristics',
+                    hue=df_clustered['Cluster'])
     plt.title('PCA Clusters')
     plt.show()
 
@@ -27,6 +29,7 @@ def main():
         df_cluster = df_clustered[df_clustered['Cluster'] == cluster]
         visualize_distributions(df_cluster, cluster)
         chi_square_test(df_cluster, cluster)
+
 
 if __name__ == "__main__":
     main()
